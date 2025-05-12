f37c0b033a6f47834dcbfc02063c64306d95b971
Lucas Castro <169384752+LuquinhasCTR@users.noreply.github.com>
2025-05-12 10:52:00 +0100

    Refactor na inicialização

--- Escola/Form1.cs
+++ Escola/Form1.cs
@@ -15,6 +15,8 @@ namespace Escola
 {
     public partial class Form1 : Form
     {
+        /// <summary>Dados dos alunos. </summary>
+        private List<Aluno> Alunos;
         public Form1()
         {
             InitializeComponent();
@@ -22,11 +24,24 @@ namespace Escola
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Inicializar a ListView
+            ListaAlunos.View = View.Details;
+            ListaAlunos.FullRowSelect = true;
+            ListaAlunos.Columns.Add("Processo", 60, HorizontalAlignment.Left);
+            ListaAlunos.Columns.Add("Número", 50, HorizontalAlignment.Left);
+            ListaAlunos.Columns.Add("Nome", 120, HorizontalAlignment.Left);
+            ListaAlunos.Columns.Add("Data Nascimento", -2, HorizontalAlignment.Left);
+
+            Inicializar();
+        }
+        private void Inicializar()
+        {
+
             using (var connection = new MySqlConnection(LigacaoDB.GetConnectionString()))
             {
                 int n = connection.ExecuteScalar<int>("SELECT COUNT(*) FROM alunos");
                 NumeroRegistos.Text = $"{n} alunos";
-            }
+            }   
         }
     }
 }
